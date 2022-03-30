06edda7664b701c0c26980b35317406c5dea9ba1
panshulbh16 <bharadwajpanshul@gmail.com>
2022-03-31 00:03:34 +0530

    My first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,47 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="img">
+      <img src="nee.jpeg" width="200px" classname = "ima"></img> 
+       </div>
+       <div className="info">
+         <div className="title">
+          <h1>Neha Raikar</h1>
+          <h5>
+          <button type="button" class="btn-email"> <a href ="https://www.linkedin.com/in/neha-raikar/">
+          Software Engineer</a> </button> 
+           </h5>
+         </div>
+         <div className="buttons">
+          <button type="button" class="btn-email"> <a href ="mailto:nehagraikar@gmail.com">
+            E-mail</a></button> 
+            
+            <button type="button" class="btn-email"> <a href ="https://www.linkedin.com/in/neha-raikar/">
+            LinkedIN</a></button> 
+         </div>
+         <div className="text">
+         <p className="aboutme"> <center> |Software Engineer at Amazon India|<br></br>|Completed Bachelors of Technology from Ramaiah University|<br></br>
+         |Always happy to contribute in different open-source projects|</center> </p> </div>
+           <h3 className="interest"> Interests </h3>
+           <p className="aboutint">Arts || Tech || Netflix || Travel</p>
+       </div>
+       <div className="footer">
+         <div className="socialicons">
+         <button type="button" class="btn"> <a href ="https://leetcode.com/neharaikar/">
+            LeetCode</a></button> 
+            <button type="button" class="btn"> <a href ="https://www.instagram.com/neharaikar.art/">
+            ArtPage</a></button> 
+            <button type="button" class="btn"> <a href ="https://github.com/neharaikar">
+            Github</a></button> 
+         </div>
+         <div className="res">
+         <button type="button" class="btn"> <a href ="https://acrobat.adobe.com/link/track?uri=urn:aaid:scds:US:612c8380-f6fd-38f5-95be-836f5fcca983">Resume</a></button>
+         </div>
+       </div>
+       <div className="footer">
+         <p className="foo">
+         Contact No:+91-9110289653 || Bangalore,IN</p></div>
     </div>
   );
 }
